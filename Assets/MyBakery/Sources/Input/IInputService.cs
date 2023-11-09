7f58c671418d53f68924e3a29159119620158b80
Virvon <dan130205@gmail.com>
2023-11-09 10:57:10 +0300

    Added Joystic

---
+++ Assets/MyBakery/Sources/Input/IInputService.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public interface IInputService
+{
+    Vector2 Direction { get; }
+}
+
+public class InputService : IInputService
+{
+    public Vector2 Direction { get; private set; }
+}
---
+++ Assets/MyBakery/Sources/Input/Joystick/Joystick.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] private RectTransform _slidingArea;
+    [SerializeField] private RectTransform _handleBackgorund;
+    [SerializeField] private RectTransform _handle;
+
+    private Vector2 _startPosition;
+
+    public Vector2 Direction { get; private set; }
+
+    private void Start()
+    {
+        _startPosition = _handleBackgorund.anchoredPosition;
+        _handleBackgorund.gameObject.SetActive(false);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 pointPosition;
+
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(_slidingArea, eventData.position, null, out pointPosition))
+        {
+            _handleBackgorund.gameObject.SetActive(true);
+            _handleBackgorund.anchoredPosition = pointPosition;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _handleBackgorund.anchoredPosition = _startPosition;
+        _handle.anchoredPosition = Vector2.zero;
+        _handleBackgorund.gameObject.SetActive(false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 handlePosititon;
+
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(_handleBackgorund, eventData.position, null, out handlePosititon))
+        {
+            handlePosititon = handlePosititon * 2 / _handleBackgorund.sizeDelta;
+
+            if (handlePosititon.magnitude > 1)
+                handlePosititon.Normalize();
+
+            _handle.anchoredPosition = handlePosititon * _handleBackgorund.sizeDelta / 2;
+            
+            if(handlePosititon != Vector2.zero)
+            {
+                Debug.Log(handlePosititon);
+            }
+        }
+    }
+}
